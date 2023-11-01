590deb0d0d54a6cc15c40e6bbc7e6593b41d678d
Hee Su Chung <jhschung88@gmail.com>
2023-11-01 16:34:48 -0400

    undid the changes i accidentally committed

--- PaneOrganizer.java
+++ PaneOrganizer.java
@@ -18,7 +18,7 @@ public class PaneOrganizer {
     this.root.setCenter(alienPane);
     HBox buttonPane = new HBox();
     this.root.setBottom(buttonPane);
-    this.alien = new Alien(alienPane);
+    Alien alien = new Alien(alienPane);
     this.setUpButtons(buttonPane);
   }
 
@@ -33,14 +33,11 @@ public class PaneOrganizer {
     buttonPane.setAlignment(Pos.CENTER);
     b1.setOnAction((ActionEvent e) -> this.alien.moveLeft());
     b2.setOnAction((ActionEvent e) -> this.alien.moveRight());
-//    System.out.println("b1: " + b1);
-//    System.out.println("b2: " + b2);
-//    System.out.println("alien: " + this.alien);
     b3.setOnAction((ActionEvent e) -> a.printArray());
     b4.setOnAction((ActionEvent e) -> a.changeArrayList());
   }
 
-  public Pane getRoot() {
+  public Pane getRoots() {
     return this.root;
   }
 }
